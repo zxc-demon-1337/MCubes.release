bebfaab28f9359c7f53fe43e263ac46a033b2706
zxc-demon-1337 <227mr123@mail.ru>
2026-04-16 15:09:19 +0300

    Update settings.py

--- MCubes/settings.py
+++ MCubes/settings.py
@@ -141,12 +141,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+
 
 USE_I18N = True
 
 USE_TZ = True
-
+TIME_ZONE = 'Europe/Moscow'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
